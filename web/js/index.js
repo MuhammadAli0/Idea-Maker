6589c551d51e01e34ffb414efba7474be121174f
Mohammed Ali <mohammedali@technologist.com>
2019-01-25 23:15:41 +0200

    Fix Cashe

--- web/js/index.js
+++ web/js/index.js
@@ -591,13 +591,8 @@ $(document).ready(function () {
                         GetProfilePicture = (result['personal']['gender'] === "male") ? "img/M_profile.png" : "img/F_profile.png";
                         var Home_html = `
 
-                        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-                        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-                        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-                        <!------ Include the above in your HEAD tag ---------->
                         
                         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-                        <link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css">
                         <div class="mainbody container-fluid">
                             <div class="row" style="flex-wrap: unset;">
                                
@@ -1135,9 +1130,6 @@ $(document).ready(function () {
 
     function ShowSendRecoverMailForm() {
         var html = `
-            <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-                <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-                <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
                 <!------ Include the above in your HEAD tag ---------->
                 
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
